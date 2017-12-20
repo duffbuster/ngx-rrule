e82edf4bfd0e40c70731e4e9f0f771a3d17406e7
Colin Mackey <colin.mackey@udig.com>
2017-12-20 18:08:27 -0500

    Adding config files

---
+++ build.ts
@@ -0,0 +1,185 @@
+/* tslint:disable:import-blacklist */
+// from https://github.com/angular/angularfire2/blob/master/tools/build.js
+import { spawn } from 'child_process';
+// tslint:disable:no-implicit-dependencies
+import * as copyfiles from 'copy';
+import { copy } from 'fs-extra';
+import { rollup } from 'rollup';
+import * as filesize from 'rollup-plugin-filesize';
+import * as sourcemaps from 'rollup-plugin-sourcemaps';
+import { Observable } from 'rxjs';
+
+const pkg = require(`${process.cwd()}/package.json`);
+
+// Rollup globals
+const GLOBALS = {
+  '@angular/core': 'ng.core',
+  '@angular/common': 'ng.common',
+  '@angular/animations': 'ng.animations',
+  '@angular/platform-browser': 'ng.platformBrowser',
+  'rxjs/Observable': 'Rx',
+  'rxjs/Subject': 'Rx',
+  'rxjs/Observer': 'Rx',
+  'rxjs/Subscription': 'Rx'
+};
+
+// Constants for running typescript commands
+const NGC = './node_modules/.bin/ngc';
+const TSC_ARGS = (config = 'build'): Array<any> => [
+  '-p',
+  `${process.cwd()}/src/lib/tsconfig-${config}.json`
+];
+
+/**
+ * @description Create an Observable of a spawned child process.
+ */
+function spawnObservable(command, args): void {
+  return Observable.create(observer => {
+    const cmd = spawn(command, args);
+    observer.next(''); // hack to kick things off, not every command will have a stdout
+    cmd.stdout.on('data', data => {
+      observer.next(data.toString());
+    });
+    cmd.stderr.on('data', data => {
+      observer.error(data.toString());
+    });
+    cmd.on('close', data => {
+      observer.complete();
+    });
+  });
+}
+
+function generateBundle(input, file, globals, name, format): any {
+  const plugins = [sourcemaps(), filesize()];
+
+  return rollup({
+    input,
+    external: Object.keys(globals),
+    file,
+    plugins,
+    onwarn(warning): void {
+      if (warning.code === 'THIS_IS_UNDEFINED') {
+        return;
+      }
+      if (warning.code === 'UNUSED_EXTERNAL_IMPORT') {
+        return;
+      }
+      console.log(warning.message);
+    }
+  }).then(bundle => {
+    return bundle.write({
+      file,
+      name,
+      globals,
+      format,
+      sourcemap: true
+    });
+  });
+}
+
+function createUmd(globals): any {
+  const name = 'ngx-rrule';
+  const entry = `${process.cwd()}/dist/es5/index.js`;
+
+  return generateBundle(
+    entry,
+    `${process.cwd()}/dist/packages-dist/rrule.umd.js`,
+    globals,
+    name,
+    'umd'
+  );
+}
+
+function createEs(globals, target): any {
+  const name = 'ngx-rrule';
+  const entry = `${process.cwd()}/dist/${target}/index.js`;
+
+  return generateBundle(
+    entry,
+    `${process.cwd()}/dist/packages-dist/rrule.${target}.js`,
+    globals,
+    name,
+    'es'
+  );
+}
+
+function getVersions(): any {
+  const paths = [`${process.cwd()}/dist/packages-dist/package.json`];
+
+  return paths.map(path => require(path)).map(pkgs => pkgs.version);
+}
+
+function verifyVersions(): any {
+  const versions = getVersions();
+  console.log(versions);
+  versions.map(version => {
+    if (version !== pkg.version) {
+      throw new Error('Versions mistmatch');
+    }
+  });
+}
+
+function buildModule(globals): any {
+  const es2015$ = spawnObservable(NGC, TSC_ARGS()) as any;
+  const esm$ = spawnObservable(NGC, TSC_ARGS('esm')) as any;
+
+  return Observable.forkJoin(es2015$, esm$);
+}
+
+function createBundles(globals): any {
+  return Observable.forkJoin(
+    Observable.from(createUmd(globals)),
+    Observable.from(createEs(globals, 'es2015')),
+    Observable.from(createEs(globals, 'es5'))
+  );
+}
+
+function copyFiles(): any {
+  const copyAll: ((s: string, s1: string) => any) = Observable.bindCallback(
+    copyfiles
+  );
+
+  return Observable.forkJoin(
+    copyAll(
+      `${process.cwd()}/dist/es2015/**/*.d.ts`,
+      `${process.cwd()}/dist/packages-dist`
+    ),
+    copyAll(
+      `${process.cwd()}/dist/es2015/**/*.metadata.json`,
+      `${process.cwd()}/dist/packages-dist`
+    ),
+    Observable.from(
+      copy(
+        `${process.cwd()}/README.md`,
+        `${process.cwd()}/dist/packages-dist/README.md`
+      )
+    ),
+    Observable.from(
+      copy(
+        `${process.cwd()}/src/lib/package.json`,
+        `${process.cwd()}/dist/packages-dist/package.json`
+      )
+    ),
+    Observable.from(
+      copy(
+        `${process.cwd()}/src/lib/rrule.css`,
+        `${process.cwd()}/dist/packages-dist/rrule.css`
+      )
+    )
+  );
+}
+
+function buildLibrary(globals): any {
+  const modules$ = buildModule(globals);
+
+  return Observable.forkJoin(modules$)
+    .switchMap(() => createBundles(globals))
+    .switchMap(() => copyFiles())
+    .do(() => verifyVersions());
+}
+
+buildLibrary(GLOBALS).subscribe(
+  data => console.log('success'),
+  err => console.log('err', err),
+  () => console.log('complete')
+);
